5784a28d8846985816c638da6323c961a348a991
Muxxs <muxxsmu@outlook.com>
2020-07-23 19:40:43 +0800

    Sixth Version on 7-23
    Turn to mysql

--- operation.py
+++ operation.py
@@ -1,84 +1,126 @@
 # coding=utf-8
 from tinydb import TinyDB
 from tinydb.queries import Query, where
+import pymysql
+
+Host = "140.143.3.101"
+UserName = 'Zongji'
+Passwd = "DwybnAKDSwhk6NWc"
+DB = 'Zongji'
+conn = pymysql.connect(host=Host, user=UserName, passwd=Passwd, db=DB, charset='utf8')
 
 
 class ConfigDBoperate:  # Data for json's
     def __init__(self):
         self.data = TinyDB('config/config.json')
         self.table = self.data.table('config')
+        self.cursor = conn.cursor()
 
     def GetAllUserID(self):
-        Q = Query()
-        return self.table.search(Q.Name == 'UserID')[0]['Config']
+        SQL = 'SELECT * FROM Config where Name = "UserID"'
+        self.cursor.execute(SQL)
+        return self.cursor.fetchall()[0][1]
 
     def GetAllInforID(self):
-        Q = Query()
-        return self.table.search(Q.Name == 'InforID')[0]['Config']
+        SQL = 'SELECT * FROM Config where Name = "InforID"'
+        self.cursor.execute(SQL)
+        return self.cursor.fetchall()[0][1]
 
     def GetAllPassID(self):
-        Q = Query()
-        return self.table.search(Q.Name == 'PassID')[0]['Config']
+        SQL = 'SELECT * FROM Config where Name = "PassID"'
+        self.cursor.execute(SQL)
+        return self.cursor.fetchall()[0][1]
 
     def CreateTable(self):
-        self.table.insert({"Name": "UserID", "Config": 0})
-        self.table.insert({"Name": "InforID", "Config": 0})
-        self.table.insert({"Name": "PassID", "Config": 0})
+        SQL = 'INSERT INTO Config (Name , Content) VALUES ("UserID", 0 )'
+        self.cursor.execute(SQL)
+        SQL = 'INSERT INTO Config (Name , Content) VALUES ("PassID", 0 )'
+        self.cursor.execute(SQL)
+        SQL = 'INSERT INTO Config (Name , Content) VALUES ("InforID", 0 )'
+        self.cursor.execute(SQL)
 
     def UserIDPlus(self):
-        self.table.update({"Config": self.GetAllUserID() + 1}, where('Name') == "UserID")
+        Now = self.GetAllUserID()
+        SQL = 'UPDATE Config SET Content = %d WHERE Name="UserID"' % (Now + 1)
+        self.cursor.execute(SQL)
+        conn.commit()
 
     def InforIDPlus(self):
-        self.table.update({"Config": self.GetAllInforID() + 1}, where('Name') == "InforID")
+        Now = self.GetAllInforID()
+        SQL = 'UPDATE Config SET Content = %d WHERE Name="InforID"' % (Now + 1)
+        self.cursor.execute(SQL)
+        conn.commit()
 
     def PassIDPlus(self):
-        self.table.update({"Config": self.GetAllPassID() + 1}, where('Name') == "PassID")
+        Now = self.GetAllUserID()
+        SQL = 'UPDATE Config SET Content = %d WHERE Name="PassID"' % (Now + 1)
+        self.cursor.execute(SQL)
+        conn.commit()
 
 
 class InfroDBoperate:  # Scenic spot Information
     def __init__(self):
-        self.data = TinyDB('config/beijing.json')
-        self.table = self.data.table('config')
+        self.cursor = conn.cursor()
 
     def Insert(self, name: str, tag: []):
+        list = ''
+        for i in tag: list += str(i) + "-"
+        list = list[:-1]
+        A = ConfigDBoperate()
+        SQL = 'INSERT INTO Beijing (ID,name, tag) VALUES (%d, "%s","%s")' % (
+            A.GetAllInforID() + 1, name, list)
+        self.cursor.execute(SQL)
+        conn.commit()
         D = ConfigDBoperate()
         D.InforIDPlus()
-        self.table.insert({'id': D.GetAllInforID(), 'name': name, 'tag': tag})
 
     def Search(self, id: int):
-        Q = Query()
-        return self.table.search(Q.id == int(id))[0]
+        SQL = 'SELECT * FROM Beijing where ID = %d' % id
+        self.cursor.execute(SQL)
+        return self.cursor.fetchall()[0]
 
 
 class UserDBoperate:  # User Database Operations
     def __init__(self):
-        self.data = TinyDB('config/user.json')
-        self.table = self.data.table('user')
+        self.cursor = conn.cursor()
 
     def Insert(self, user: str, pw: str, like: []):  # Sign Up
+        list = ''
+        for i in like: list += str(i) + "-"
+        list = list[:-1]
+        A = ConfigDBoperate()
+        SQL = 'INSERT INTO Beijing (ID,User, pw,like) VALUES (%d, "%s","%s","%s")' % (
+            A.GetAllUserID() + 1, user, pw, list)
+        self.cursor.execute(SQL)
+        conn.commit()
         D = ConfigDBoperate()
         D.UserIDPlus()
-        self.table.insert({"id": D.GetAllUserID(), "user": user, 'pw': pw, 'like': like})
 
     def Search(self, id: int):  # Get information
-        Q = Query()
-        return self.table.search(Q.id == int(id))[0]
+        SQL = 'SELECT * FROM User where ID = %d' % id
+        self.cursor.execute(SQL)
+        return self.cursor.fetchall()[0]
 
 
-class PassageDBoperate: # Passages Database Operations
+class PassageDBoperate:  # Passages Database Operations
     def __init__(self):
-        self.data = TinyDB('config/passage.json')
-        self.table = self.data.table('user')
+        self.cursor = conn.cursor()
 
     def Insert(self, passage: str, AuthorID: int, time: str, PlaceID: []):
+        list = ''
+        for i in PlaceID: list += str(i) + "-"
+        list = list[:-1]
+        SQL = 'INSERT INTO Passage (PassageID,AuthorID, Time,PlaceID,Passage) VALUES (%d, %d,"%s","%s","%s")' % (
+            A.GetAllPassID() + 1, AuthorID, time, list, passage)
+        self.cursor.execute(SQL)
+        conn.commit()
         D = ConfigDBoperate()
         D.PassIDPlus()
-        self.table.insert(
-            {"PassageID": D.GetAllPassID(), "AuthorID": AuthorID, "Time": time, "PlaceID": PlaceID, "Passage": passage})
 
     def Search(self, id: int):
-        Q = Query()
-        return self.table.search(Q.AuthorID == int(id))[0]
+        SQL = 'SELECT * FROM Passage where PassageID = %d' % id
+        self.cursor.execute(SQL)
+        return self.cursor.fetchall()[0]
 
 
 class Show:  # To Show all information
@@ -100,11 +142,3 @@ class Show:  # To Show all information
     def AllInfor(self):  # Show every Information
         for i in range(1, self.InforID + 1):
             print(self.Infor.Search(i))
-
-
-P = PassageDBoperate()
-print(P.Search(1)['AuthorID'])
-A = Show()
-A.GetIDs()
-A.AllUser()
-A.AllInfor()
